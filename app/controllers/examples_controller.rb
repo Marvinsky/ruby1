5aeabd08e2c811890e579a6217e22b5f7a3ca3ac
Marvinsky <nivramsky@gmail.com>
2015-11-07 19:24:09 -0200

    07/11/15 insert

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,5 +1,7 @@
 class ExamplesController < ApplicationController
   def form
   	@nombre = params[:nombre_usuario]
+  	#@ejemplo = Example.create({nombre: @nombre})
+  	@ejemplo = Example.create({nombre: @nombre})
   end
 end
---
+++ app/models/example.rb
@@ -0,0 +1,5 @@
+class Example < ActiveRecord::Base
+
+	# attr_accessible :nombre -> not available for Rails version 4+
+	
+end
